d4df02c65869f4f680fb21a9e5fe161b9628ef41
ConnieZi <conniezi1008@gmail.com>
2023-06-04 17:00:25 -0400

    Create a security group

--- main.tf
+++ main.tf
@@ -14,11 +14,58 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+# set up a security group for our EC2 instance
+# 1. set up a vpc, which is the network infra provided by Amazon
+data "aws_vps" "default" {
+  default = true
+}
+
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = ["aws_security_group.blog.id"]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+# 2. set up a security group, which is a firewall for EC2 instances
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out"
+
+  # The vpc we set up above
+  voc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"  # this is because we are setting up inbound rules
+  from_port   = 80    # port 80 is for http
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # which networks we want to allow access on, and here it's a wide-open public website
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"  # this is because we are setting up inbound rules
+  from_port   = 443    # port 80 is for http
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # which networks we want to allow access on, and here it's a wide-open public website
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everythin_out" {
+  type        = "egress"  # this is because we are setting up inbound rules
+  from_port   = 00        # allow everything outbound
+  to_port     = 00
+  protocol    = "-1"      # allow all protocols
+  cidr_blocks = ["0.0.0.0/0"]  # which networks we want to allow access on, and here it's a wide-open public website
+
+  security_group_id = aws_security_group.blog.id
 }
