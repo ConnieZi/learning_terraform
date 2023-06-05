2f8f73bba502fc675a53b8707f8680c4c73ba6f7
ConnieZi <conniezi1008@gmail.com>
2023-06-05 11:56:43 -0400

    Create dev vpc

--- main.tf
+++ main.tf
@@ -14,7 +14,23 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-# set up a security group for our EC2 instance
+# use the vpc module from the terraform registry instead of writing our own
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+# (deprecated) set up a security group for our EC2 instance
 # 1. set up a vpc, which is the network infra provided by Amazon
 data "aws_vpc" "default" {
   default = true
@@ -38,7 +54,7 @@ module "blog_sg" {
   name    = "blog_new"
 
   # The vpc we set up above
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0] # just use the first public subnet
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
