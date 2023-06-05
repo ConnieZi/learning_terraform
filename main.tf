8025b6b71120f1fb601461f5fe789046176bc084
ConnieZi <conniezi1008@gmail.com>
2023-06-05 14:33:18 -0400

    Autoscaling

--- main.tf
+++ main.tf
@@ -36,20 +36,37 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+# resource "aws_instance" "blog" {
+#   ami           = data.aws_ami.app_ami.id
+#   instance_type = var.instance_type
 
-  # look for the output of this module in the documentation outputs section
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+#   # look for the output of this module in the documentation outputs section
+#   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "LearningTerraform"
-  }
+#   tags = {
+#     Name = "LearningTerraform"
+#   }
+# }
+
+# this will late replace the above aws_instance resource and autoscale the EC2 instances
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   =  module.blog_alb.target_group_arns  # target group is created by the load balancer, it's what the traffic is targeted to
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id       = data.aws_ami.app_ami.id
+  instance_type  = var.instance_type
 }
 
 # Application Load Balancer
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -57,9 +74,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id           = module.blog_vpc.vpc_id
+  subnets          = module.blog_vpc.public_subnets
+  security_groups  = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
